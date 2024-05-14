0d2096782fbd3a376030c3105821f1f1fec5f008
yevavorov <yevavorov-ipt27@lll.kpi.ua>
2024-05-14 15:42:14 +0300

    added extra

--- lab_6/laba6_2.py
+++ lab_6/laba6_2.py
@@ -15,7 +15,10 @@ def gradient_descent(x, y, learning_rate, n_iter):
     k = 0
     b = 0
     n = len(x)
+    break_stop = False
     mse_history = []
+    global max_mse
+    max_mse = n_iter
     for _ in range(n_iter):
         y_pred = k*x + b
         k_grad = (-2/n) * np.sum(x * (y - y_pred))
@@ -24,6 +27,9 @@ def gradient_descent(x, y, learning_rate, n_iter):
         b -= learning_rate * b_grad
         mse = mean_squared_error(x, y, k, b)
         mse_history.append(mse)
+        if _ > 0 and np.abs(mse_history[_] - mse_history[_-1]) <= 1e-4 and break_stop == False:
+            break_stop = True
+            max_mse = len(mse_history)
     return k, b, mse_history
 
 # y = kx + b
@@ -50,6 +56,7 @@ true_k = 2
 true_b = 5
 num_points = 100
 noise_level = 2
+max_mse = 0
 x, y = generate_data(num_points, true_k, true_b, noise_level)
 grad_k, grad_b, mse_history = gradient_descent(x, y, learning_rate, n_iter)
 
@@ -83,22 +90,32 @@ text_box = TextBox(ax_text, "Results:", initial=text_results)
 
 # Checkbox для ліній
 ax_MNK = plt.axes([0.71, 0.7, 0.19, 0.26])
-check_lines = CheckButtons(ax_MNK, ["МНК", "np.polyfit", "Град. спуск"], [True, True, True])
+check_lines = CheckButtons(ax_MNK, ["МНК", "np.polyfit", "Град. спуск", "Зупинка"], [True, True, True, True])
 
-# Slider для n_iter
-ax_n_iter = plt.axes([0.75, 0.64, 0.15, 0.05])
+# Slider для n_iter та learning_rate
+ax_n_iter = plt.axes([0.75, 0.665, 0.15, 0.025])
 s_n_iter = Slider(ax_n_iter, "n_iter", valmin=100, valmax=1000, valstep=100, valinit=n_iter)
+ax_learning_rate = plt.axes([0.75, 0.645, 0.15, 0.025])
+s_learning_rate = Slider(ax_learning_rate, "learning_rate", valmin=0.001, valmax=0.01, valstep=0.001, valinit=learning_rate)
 
 # Update (оновлення параметрів)
 def update(val):
     MNK_status = check_lines.get_status()[0]
     polyfit_status = check_lines.get_status()[1]
     grad_status = check_lines.get_status()[2]
+    stop_status = check_lines.get_status()[3]
     new_n_iter = s_n_iter.val
-    new_grad_k, new_grad_b, new_mse_history = gradient_descent(x, y, learning_rate, new_n_iter)
+    new_learning_rate = s_learning_rate.val
+    new_grad_k, new_grad_b, new_mse_history = gradient_descent(x, y, new_learning_rate, new_n_iter)
     linereg.set_ydata(new_grad_k*x + new_grad_b)
+    linemse.set_xdata(range(1, new_n_iter + 1))
+    linemse.set_ydata(new_mse_history)
+    linemse_stop.set_xdata(range(1, max_mse + 1))
+    linemse_stop.set_ydata(new_mse_history[:max_mse])
     new_p = f"[{new_n_iter}] Похибка: {new_mse_history[new_n_iter-1]}"
     p_text.set_text(new_p)
+    new_mm = f"[{max_mse}/{new_n_iter}] Зупинка: {new_mse_history[max_mse-1]}"
+    mm_text.set_text(new_mm)
     new_text_results = f"""Метод найменших квадратів (МНК):
     k: {mnk_k}
     b: {mnk_b}
@@ -127,10 +144,15 @@ np.polyfit:
         linereg.set_visible(True)
     else:
         linereg.set_visible(False)
+    if stop_status:
+        linemse_stop.set_visible(True)
+    else:
+        linemse_stop.set_visible(False)
     fig.canvas.draw_idle()
 
 check_lines.on_clicked(update)
 s_n_iter.on_changed(update)
+s_learning_rate.on_changed(update)
 
 
 # Графік
@@ -139,9 +161,13 @@ line, = ax[0].plot(x, true_k*x + true_b, color="red", label="Пряма")
 lineMNK, = ax[0].plot(x, mnk_k*x + mnk_b, color="green", label="МНК")
 linepoly, = ax[0].plot(x, polyfit_k*x + polyfit_b, color="blue", label="np.polyfit")
 linemse, = ax[1].plot(range(1, n_iter + 1), mse_history, color="magenta")
+linemse_stop, = ax[1].plot(range(1, max_mse + 1), mse_history[:max_mse], color="cyan")
 linereg, = ax[0].plot(x, grad_k*x + grad_b, color="orange", label="Град. спуск")
 ax[0].legend()
 ax[0].grid(color="0.8")
+ax[1].grid(color="0.8")
 p = f"[{n_iter}] Похибка: {mse_history[n_iter-1]}"
+mm = f"[{max_mse}/{n_iter}] Зупинка: {mse_history[max_mse-1]}"
 p_text = ax[1].text(150, 32, p, fontsize=12)
+mm_text = ax[1].text(130, 30, mm, fontsize=12)
 plt.show()
